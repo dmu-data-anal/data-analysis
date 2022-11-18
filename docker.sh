06a84619dabcb950a738fe18034a6bd32a79ed8c
echo2719 <echo2719@dongyang.ac.kr>
2022-11-18 13:55:59 +0000

    removed

--- docker.sh
+++
@@ -1,3 +0,0 @@
-#sudo docker container run -it --user 0 -v /mnt/d/data-analysis/:/home/jovyan/works -p 8888:8888 -e GRANT_SUDO=yes --name jupyterlab-sudo my-jupyterlab bash
-#sudo docker container run -it --user 0 -p 8888:8888 -e GRANT_SUDO=yes --name jupyterlab-sudo little7279/my-jupyterlab:0.1a bash
-sudo docker container run -it --user 0 -p 8888:8888 -e GRANT_SUDO=yes --name <name> little7279/my-jupyterlab:<tags> bash
