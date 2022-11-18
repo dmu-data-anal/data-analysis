9d2214495c6b7c61e073398446e23d69921cc691
Jonggwan Park <30569896+little7279@users.noreply.github.com>
2022-11-18 13:45:19 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,4 @@
-sudo docker container run -it --user 0 -v /mnt/d/data-analysis/:/home/jovyan/works -p 8888:8888 -e GRANT_SUDO=yes --name jupyterlab-sudo my-jupyterlab
+#sudo docker container run -it --user 0 -v /mnt/d/data-analysis/:/home/jovyan/works -p 8888:8888 -e GRANT_SUDO=yes --name jupyterlab-sudo my-jupyterlab
+bash
+sudo docker container run -it --user 0 -p 8888:8888 -e GRANT_SUDO=yes --name jupyterlab-sudo little7279/my-jupyterlab:0.1a
 bash
